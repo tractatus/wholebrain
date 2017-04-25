784516892db5fcfb9b4a9b9f8b9bf232ddf27bf0
Daniel Fürth <daniel.furth@ki.se>
2017-04-25 16:50:27 +0200

    updated bargraph() command and moved it to plotting.R

--- R/plotting.R
+++ R/plotting.R
@@ -653,3 +653,79 @@ glassbrain<-function(dataset, high.res=FALSE, dim=c(720,1080), device=TRUE, col=
 	
 }
 
+
+
+
+
+bargraph <- function(dataset, device=TRUE) {
+  
+  
+  with(dataset, {
+     
+
+    counts<-table(as.character(acronym), right.hemisphere)
+    hemisphere.to.sort<-which.max(apply(counts, 2, sum))
+    counts <-counts[order(counts[,hemisphere.to.sort], decreasing=TRUE),]
+    counts <-log10(counts)
+    if(device){quartz(width= 7.036585, height= 0.2099039*nrow(counts))}
+    layout(matrix(c(1,1,1,2,2,2,2), nrow=1))
+    par(mar=c(4,0,4,0))
+    plot(rep(2.5, nrow(counts) ),nrow(counts):1, col=0, axes=F, ylim=c(0.5,nrow(counts)+0.5), ylab='', xlab='', xlim=c(1,5))
+   
+   #regionnames
+   mtext('Input region:',3, cex=0.9)
+   for(i in 1:nrow(counts)){
+    regioncolor<-color.from.acronym(row.names(counts)[i])
+    regioncolor<-adjustcolor( regioncolor, alpha.f = 0.1)
+    y.lab<-(nrow(counts)+1)-i
+    polygon(c(1,5,5,1), c(y.lab-0.5, y.lab-0.5, y.lab+0.5, y.lab+0.5), col= regioncolor, border=FALSE )
+    text(3, y.lab, name.from.acronym(row.names(counts)[i]), cex=0.9 )
+   }
+    par(xpd=TRUE)
+     legend(3, -0.75, c('Left', 'Right'), pch=c(21), pt.bg=c('white',gray(0.2)), title='Hemisphere:', bg='white', horiz=TRUE, cex=1.3, xjust=0.5)
+     par(xpd=FALSE) 
+   par(mar=c(4,4,4,6))
+   zeros<-min(is.finite(counts))-1
+   counts[!is.finite(counts)]<-zeros
+  x.range<-round(range(counts[is.finite(counts)]))
+  plot( apply(counts, 1, max), nrow(counts):1-0.125, pch=21, bg='white', ylim=c(0.5,nrow(counts)+0.5),  xlim=x.range, xlab='', axes=F, ylab='', col=0 )
+  
+  for(i in 1:nrow(counts)){
+    regioncolor<-color.from.acronym(row.names(counts)[i])
+    regioncolor<-adjustcolor( regioncolor, alpha.f = 0.05)
+    y.lab<-(nrow(counts)+1)-i
+    polygon(c(x.range,rev(x.range)), c(y.lab-0.5, y.lab-0.5, y.lab+0.5, y.lab+0.5), col= regioncolor, border=FALSE )
+   }
+  
+   log.range<-10^seq(x.range[1], x.range[2])
+   axis(1, at=seq(x.range[1], x.range[2]), las=1, labels=c(0,log.range[-1]) )
+   axis(3, at=seq(x.range[1], x.range[2]), las=1, labels=c(0,log.range[-1]) )
+
+  
+   log.range <-unlist(lapply(1:(length(log.range)-1), function(x){seq(log.range[x], log.range[x+1], by=log.range[x])}))
+   
+  axis(1, at=log10(log.range), labels=FALSE)
+  axis(3, at=log10(log.range), labels=FALSE)
+  axis(2, at=nrow(counts):1, labels=row.names(counts), las=1)
+  axis(4, at=nrow(counts):1, labels=row.names(counts), las=1)
+    
+    abline(h=1:nrow(counts), lty=2, col='gray')
+    abline(v=log10(log.range), col='lightblue')
+    
+    lapply(1:nrow(counts), function(x){lines(counts[x,], rep(nrow(counts)-x+1,2), lwd=2)})
+    points(counts[,(hemisphere.to.sort==2)+1], nrow(counts):1, pch=21, bg=gray(0.2), cex=1.2)
+    points(counts[, (hemisphere.to.sort==2)+0], nrow(counts):1, pch=21, bg='white', cex=1.2)
+
+    
+    box()
+       par(xpd=TRUE)
+   polygon(c(x.range[1]+0.25, x.range[1]+0.5, x.range[1]+0.5, x.range[1]+0.25), c(-2,-2,nrow(counts)+3,nrow(counts)+3), col='white', border='white')
+  par(xpd=FALSE)
+  abline(v=c(x.range[1]+0.25, x.range[1]+0.5))
+  
+  mtext('Cell count', 3, 2.2, cex=0.8)
+
+  mtext('Cell count', 1, 2.2, cex=0.8)
+    
+  })
+}
