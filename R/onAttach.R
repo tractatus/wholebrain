eb26b08587e680b32d5417906f2a429e530f61f3
Daniel Fürth <daniel.furth@ki.se>
2017-06-12 12:31:17 +0200

    added quartz() to .GlobalEnv for windows for onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,5 +2,6 @@
     packageStartupMessage("WholeBrain (version 0.0.8) \"Thurstone\" \n by Daniel Fürth, 2017, by using this software you agree to the EULA")
     if(.Platform$OS.type=="windows") {
   		quartz<-function(width,height){windows(width, height)}
+  		assign("quartz", quartz, envir = .GlobalEnv) 
 	}
 }
